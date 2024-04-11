0ee8446dabe5f7624e7d3e46031102c1f0e76767
snowMan128 <p826093@gmail.com>
2024-04-11 15:51:43 +0100

    push

--- circuits/consts.rs
+++ circuits/consts.rs
@@ -2,7 +2,7 @@ pub use tendermint::merkle::HASH_SIZE;
 
 /// The number of validators on the Tendermint chain.
 /// Picasso's Testnet
-pub const VALIDATOR_SET_SIZE_MAX: usize = 15;
+pub const VALIDATOR_SET_SIZE_MAX: usize = 48;
 /// Picasso's Mainnet
 /// pub const VALIDATOR_SET_SIZE_MAX: usize = 42;
 
